c181f14d319943b9c9851a0ed217b3bd48e10b29
devEricA <eric.anderson413@gmail.com>
2022-02-28 15:03:01 -0600

    Trial 2: Using count as a global. 2 Test cases Pass

--- ParenthesisTrials.java
+++ ParenthesisTrials.java
@@ -4,8 +4,9 @@ import java.io.*;
 
 public class ParenthesisTrials{
 
+    public static int count = 0;
 
-    public static int balance(String parenthesis, int bottomPos, int topPos, int count)
+    public static int balance(String parenthesis, int bottomPos, int topPos)
     {
 
         //If the two positions have not collided, continue with the array. 
@@ -13,14 +14,16 @@ public class ParenthesisTrials{
         {
             //Taking the middle position between the top and bottom
             int middle = bottomPos + (topPos - bottomPos)/2;
-            
+
             if(parenthesis.charAt(middle) == '(')
                 count += 1;
             else if(parenthesis.charAt(middle) == ')')
                 count -= 1; 
              
-            balance(parenthesis, bottomPos, middle - 1, count);
-            balance(parenthesis, middle + 1, topPos, count);
+
+            //Going both ways in the search. 
+            balance(parenthesis, bottomPos, middle - 1);
+            balance(parenthesis, middle + 1, topPos);
         }
          
         return count;
@@ -42,12 +45,12 @@ public class ParenthesisTrials{
         file.close();
 
         //Running the method in all of the test cases
-        System.out.println(balance(caseOne, 0 , caseOne.length()-1, 0));
-        System.out.println(balance(caseTwo, 0 , caseTwo.length()-1, 0));
-        System.out.println(balance(caseThree, 0 , caseThree.length()-1, 0));
-        System.out.println(balance(caseFour, 0, caseFour.length()-1, 0));
-        System.out.println(balance(caseFive, 0, caseFive.length()-1, 0));
-        System.out.println(balance(monsterCase, 0, monsterCase.length()-1, 0));
+        System.out.println(balance(caseOne, 0 , caseOne.length()-1));
+        System.out.println(balance(caseTwo, 0 , caseTwo.length()-1));
+        System.out.println(balance(caseThree, 0 , caseThree.length()-1));
+        System.out.println(balance(caseFour, 0, caseFour.length()-1));
+        System.out.println(balance(caseFive, 0, caseFive.length()-1));
+        System.out.println(balance(monsterCase, 0, monsterCase.length()-1));
 
         
     }
